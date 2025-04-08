aa37639e7fa70f3fa0238e1abbd92d95e3910167
lorettaveiga <a00835854@tec.mx>
2025-04-07 23:37:10 -0600

    Add files via upload

---
+++ backend/controllers copy/login.controllers.js
@@ -0,0 +1,17 @@
+import { sqlConnect, sql } from "../utils/sql.js";
+
+export const login = async (req, res) => {
+  // console.log(req.body);
+  const pool = await sqlConnect();
+  const data = await pool
+    .request()
+    .input("username", sql.VarChar, req.body.username)
+    .query("select * from users where username=@username");
+  // console.log(data.recordset);
+  let isLogin = data.recordset[0].password === req.body.password;
+  if (isLogin) {
+    res.status(200).json({ isLogin: isLogin, user: data.recordset[0] });
+  } else {
+    res.status(400).json({ isLogin: isLogin, user: {} });
+  }
+};
